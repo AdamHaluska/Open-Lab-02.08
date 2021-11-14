3d57a29920e1385b2d2af7a202efa8abbae4ccb5
AdamHaluska <adam.haluska11@gmail.com>
2021-11-14 21:14:21 +0100

    hotovo!

--- Open-Lab-02.08/Checker.cs
+++ Open-Lab-02.08/Checker.cs
@@ -4,9 +4,12 @@ namespace Open_Lab_02._08
 {
     public class Checker
     {
-        public bool IsEmpty(string str)
+         if (str == "")
+                return true;
+           
         {
-            throw new NotImplementedException();
+            else
+                return false;
         }
     }
 }
